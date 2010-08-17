9c7e8ab2f4716ba78b38e30e2d99db91f571c0d2
Johannes Ebke <ebke@cern.ch>
2010-08-18 00:04:46 +0200

    removed unneccessary ipython block in mcviz __init__

--- mcviz/__init__.py
+++ mcviz/__init__.py
@@ -1,8 +1,2 @@
 from event_graph import EventGraph
 from options import parse_options
-
-try:
-    from IPython.Shell import IPShellEmbed
-    ipshell = IPShellEmbed(["-pdb"],  rc_override=dict(quiet=True))
-except ImportError:
-    ipshell = lambda: None
