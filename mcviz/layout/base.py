fc57e07d11c922c1be14c0ed3a61cefd5fed1f0b
Johannes Ebke <ebke@cern.ch>
2010-08-18 18:55:06 +0200

    factored out some feynman layout code to base class

--- mcviz/layout/base.py
+++ mcviz/layout/base.py
@@ -1,11 +1,38 @@
+from __future__ import division
+
+from ..svg import TexGlyph
+
+from ..graphviz import make_node, make_edge
+from ..utils import latexize_particle_name, make_unicode_name
+
+from math import log10
+
+class BaseLayout(object):
+
+    def __init__(self, options):
+        self.options = options
+
+    def get_label_string(self, pdgid)
+        if TexGlyph.exists(pdgid):
+            w, h = TexGlyph.from_pdgid(pdgid).dimensions
+            w *= self.options.label_size
+            h *= self.options.label_size
+            table = '<<table border="1" cellborder="0"><tr>%s</tr></table>>'
+            td = '<td height="%.2f" width="%.2f"></td>' % (h, w)
+            label = table % td
+        else:
+            # a pure number here leads graphviz to ignore the edge
+            label = "|%i|" % pdgid
 
 class LayoutEdge(object):
-    def __init__(self, spline, label = None):
+    def __init__(self, spline, label_pos=None):
         self.spline = spline
-        self.label = label
+        self.label_pos = label_pos
 
 class LayoutVertex(object):
-    def __init__(self, x = None, y = None, r = None):
+    def __init__(self, x=None, y=None, r=None):
         self.x = x
         self.y = y
         self.r = r
+
+
