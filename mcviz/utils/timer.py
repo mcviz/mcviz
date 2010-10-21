61643769693e94a77a6929df1bfcd09b0f9f1f8e
Peter Waller <peter.waller@cern.ch>
2010-10-21 04:10:25 +0200

    Add some depth information to the timer

--- mcviz/utils/timer.py
+++ mcviz/utils/timer.py
@@ -3,12 +3,18 @@ from time import time
 
 from .logger import get_logger; log = get_logger("timing")
 
+timer_depth = 0
+
 @contextmanager
 def timer(what, level=None):
+    global timer_depth
+    timer_depth += 1
     start = time()
     try: yield
     finally:
+        timer_depth -= 1
         elapsed = time() - start
-        msg = "it took %.3f seconds to %s" % (elapsed, what)
+        depth = "-" * timer_depth
+        msg = "%sit took %.3f seconds to %s" % (depth, elapsed, what)
         log.debug(msg) if level is None else log.log(level, msg)
 
