35edb04cadcea7be04e08001a08073885e394092
Peter Waller <peter.waller@cern.ch>
2010-10-29 02:17:07 +0200

    symlink_demos now takes an input directory parameter.

--- bin/symlink_demos.sh
+++ bin/symlink_demos.sh
@@ -1,14 +1,15 @@
 #! /usr/bin/env bash
 
 # Create directories for each tool type, and symlink files into them
+INDIR=$1
 
-for TOOLTYPES in $(ls -1 | awk -F- '{ print $(NF-1)"-"$NF }' | cut -d. -f1 | sort -u)
+for TOOLTYPES in $(ls -1 $INDIR | awk -F- '{ print $(NF-1)"-"$NF }' | cut -d. -f1 | sort -u)
 do
     echo Processing $TOOLTYPES
     DIR=links/${TOOLTYPES/-/\/}
     mkdir -p $DIR
-    for file in $(ls -1 | grep $TOOLTYPES)
-        do ln -s $(pwd)/$file $DIR/
+    for file in $(ls -1 $INDIR | grep $TOOLTYPES)
+        do ln -s $(pwd)/$INDIR/$file $DIR/
     done
 done
 
