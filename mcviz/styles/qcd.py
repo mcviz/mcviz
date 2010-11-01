60fa4e0234d729ab390025c9ae8a35b10de667b3
Peter Waller <peter.waller@cern.ch>
2010-11-01 20:40:12 +0100

    Move rainbow_color into styles/__init__.py so we can use it elsewhere

--- mcviz/styles/qcd.py
+++ mcviz/styles/qcd.py
@@ -2,6 +2,7 @@ from __future__ import division
 
 from random import shuffle, seed
 
+
 def qcd_rgb(layout):
     colors = [e.item.color for e in layout.edges]
     anticolors = [e.item.anticolor for e in layout.edges]
@@ -11,6 +12,9 @@ def qcd_rgb(layout):
     set_color(layout, cmap, amap) 
 
 def qcd_rainbow(layout):
+    # In here to prevent circular import
+    from mcviz.styles import rainbow_color
+    
     colors = [e.item.color for e in layout.edges]
     anticolors = [e.item.anticolor for e in layout.edges]
     unique_colors = sorted(list(set(colors + anticolors)))
@@ -37,39 +41,4 @@ def set_color(layout, cmap, amap):
                 edge.style_args["fill"] = amap[particle.anticolor]
 
 
-def rainbow_color(hue, brightness = 0.5):
-    """ get hue spectrum; hue in [0,1]:
-        red ff-ff; green 00-ff; blue 00-00
-        red ff-00; green ff-ff; blue 00-00
-        red 00-00; green ff-ff; blue 00-ff
-        red 00-00; green ff-00; blue ff-ff
-        red 00-ff; green 00-00; blue ff-ff
-        red ff-ff; green 00-00; blue ff-00
-    """
-    min_val = 0x00 if brightness < 0.5 else 2 * (brightness - 0.5) * 255
-    max_val = 0xff if brightness > 0.5 else 2 * brightness * 255
-    c_range = max_val - min_val
-
-    num = int(hue * 6)
-    remainder = hue - num/6
-    fixed_color = ((num + 1) // 2) % 3
-    run_up = (num % 2 == 0)
-    run_color = (num // 2 + 1 - (0 if run_up else 1)) % 3
-
-    def get_val(color):
-        if color == fixed_color:
-            return max_val
-        elif color == run_color:
-            if run_up:
-                return min_val + c_range * remainder
-            else:
-                return max_val - c_range * remainder
-            return c_val
-        else:
-            return min_val
-
-    res =  "#" + "".join('%02x' % get_val(c) for c in (0, 1, 2))
-    #from sys import stderr
-    #print >> stderr, "%.3f %.3f => %s" % (hue,brightness, res)
-    return res
 
