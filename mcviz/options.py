05106fa737b47e0e17934fe264a3d5a47b5e577b
Johannes Ebke <ebke@cern.ch>
2010-12-11 22:08:29 +0100

    removed obsolete options from options.py

--- mcviz/options.py
+++ mcviz/options.py
@@ -30,37 +30,6 @@ def get_option_parser():
 
     o("", "--optionset", action="append", default=["cl"], help="Select a set of default settings to use. Currently only 'cl'")
 
-    g = OptionGroup(p, "Presentation", "Options that modify the presentation")
-    p.add_option_group(g)
-    o = g.add_option
-
-    o("--stretch", default=0.2, type=float,
-      help="for -lFixIni: Ranges from 0 to 1; 0 pulls the initial particles apart the furthest.")
-
-    o("--label-size", type=float, default=1.,
-      help="scale of the labels in the output SVG file")
-    
-    o("--extra-dot", default="",
-      help="Additional information to be inserted into the graph properties")
-      
-    g = OptionGroup(p, "Output", "Options that modify the presentation")
-    p.add_option_group(g)
-    o = g.add_option
-    #
-    # Control the output file options
-    #
-    o("-o", "--output-file", type=str, default="mcviz.svg",
-      help="Output file for graph. "
-           "Note: Currently output as SVG is best by far")
-
-    o("--ratio", type=float, help="aspect ratio of output canvas")
-      
-    o("--width", default=100, type=int, help="Width of the output file in pixels")
-
-    o("--resolution", metavar="800x400",
-            type=str, help="Resolution of the output file in pixels")
-
-      
     g = OptionGroup(p, "Debug", "These options may help in finding problems")
     p.add_option_group(g)
     o = g.add_option
@@ -91,20 +60,5 @@ def parse_options(argv=None):
     result = options, args = p.parse_args(argv)
 
     options.extra_gv_options = extra_gv_options
-
-    # resolve resolutions
-    res_x, res_y = None, None
-    if options.resolution:
-        try:
-            res_x, res_y = map(int, options.resolution.split("x"))
-        except ValueError:
-            log.fatal("resolution must be given as AxB, i.e. 800x400")
-            raise FatalError
-        options.ratio = res_y / res_x
-    elif options.width:
-        res_x = options.width
-        if options.ratio:
-            res_y = int(options.ratio*res_x)
-    options.resolution = (res_x, res_y)
 
     return result
