a569f43a3050fde9a67bbce79834fc8785c9626f
Peter Waller <peter.waller@cern.ch>
2011-03-23 00:22:25 +0100

    Minor documentation commit

--- mcviz/tools/optionsets/optionsets.py
+++ mcviz/tools/optionsets/optionsets.py
@@ -2,6 +2,9 @@ from mcviz.tools import OptionSet
 from mcviz.tools.tools import ToolSetting, tool_classes, tool_types
 
 def setdefault(tools, tool_type, default, *args, **kwargs):
+    """
+    If `tool_type` is not specified on the command-line, use `default`.
+    """
     if len(tools[tool_type]) == 0:
         tools[tool_type].append(ToolSetting(default, *args, **kwargs))
 
@@ -17,3 +20,5 @@ class CommandLineOptionSet(OptionSet):
         defstyle = "Default"
         if not defstyle in [s.name for s in tools["style"]]:
             tools["style"].insert(0, ToolSetting(defstyle))
+            
+# If we add more optionsets, please update "OptionSets" in the wiki (grep for it!), or send an email.
