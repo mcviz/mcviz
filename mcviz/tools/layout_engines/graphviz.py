0050003ec86a747a089deaff1ea401127a820331
Peter Waller <peter.waller@gmail.com>
2011-12-09 16:56:45 +0000

    Add -efdp3 and -p3

--- mcviz/tools/layout_engines/graphviz.py
+++ mcviz/tools/layout_engines/graphviz.py
@@ -163,6 +163,51 @@ class FDPEngine(GraphvizEngine):
         return "\n".join(out)
 
 
+class SFDPThreeDEngine(FDPEngine):
+    _name = "sfdp3"
+    
+    def get_graphviz_options(self):
+        """
+        Return (binary, option list)
+        """
+        options = self.options["extra"].split()
+        assert not any(options.startswith("-T") for opt in options)
+        options.append("-Tdot")
+        binary = "sfdp"
+        return binary, options
+
+    def dot(self, layout):
+        extra = [
+            "dimen = 3;"
+        ]
+        return super(SFDPThreeDEngine, self).dot(layout, extra)
+    
+    def update(self, layout, data):
+        
+        import re
+        regex = r'^\s+{0}([A-Z0-9_]+)\s+\[.*?pos="(?P<pos>[\-0-9\.,]+)".*?\];\s*$'.format(REF_PREFIX)
+        nodeexpr = re.compile(regex, re.M)
+        regex = r'^\s+{0}([A-Z0-9_]+)\s+->\s+{0}([A-Z0-9_]+).*{0}([^\s,]+).*$'.format(REF_PREFIX)
+        edgeexpr = re.compile(regex, re.M)
+        
+        node_positions = dict((ref, map(float, coord.split(",")))
+                              for ref, coord in nodeexpr.findall(data))
+                              
+        edges = edgeexpr.findall(data)
+        
+        for r1, r2, r3 in edges:
+            if r1 not in node_positions:
+                log.warning("Missing node: {0}".format(r1))
+                        
+        lines = dict((r3, (node_positions[r1], node_positions[r2]))
+                     for r1, r2, r3 in edges 
+                     if r1 in node_positions and r2 in node_positions)
+        
+        layout.is3D = True
+                
+        for edge in layout.edges:
+            edge.spline = lines.get(edge.reference, None)
+
 for le in ["neato", "sfdp", "circo", "twopi"]:
     # create class and apply "tool" decorator
     classobj(le, (GraphvizEngine,), {"_name" : le})
--- mcviz/tools/painters/painters.py
+++ mcviz/tools/painters/painters.py
@@ -2,6 +2,8 @@ from __future__ import division
 
 from .. import log; log = log.getChild(__name__)
 
+import re
+
 from os.path import basename
 
 from mcviz.tools import Painter, FundamentalTool, Arg
@@ -60,3 +62,109 @@ class DOTPainter(StdPainter, FundamentalTool):
     def __call__(self, workspace, layout):
         self.write_data("graph {\n%s\n}" % layout.dot.encode("UTF-8"))
 
+class ThreePainter(StdPainter, FundamentalTool):
+    _name = "3"
+    def __call__(self, workspace, layout):
+        
+        assert getattr(layout, "is3D", False), (
+            "You must use a layout engine with 3D co-ordinates.")
+        
+        from webcolors import name_to_hex
+        def get_color(color):
+            if not color.startswith("#"):
+                color = name_to_hex(color)
+            return "0x" + color[1:]
+                
+        object_to_layoutobject = {}
+        for e in list(layout.edges) + list(layout.nodes):
+            assert not e.item in object_to_layoutobject
+            object_to_layoutobject[e.item] = e
+        
+        BIGDEPTH = 0
+        def mark(item, depth):
+            current_depth = getattr(item, "distance_to_final_vertex", BIGDEPTH)
+            item.distance_to_final_vertex = max(depth, current_depth)
+        
+        graph = layout.graph
+        final_state = [p for p in graph.particles if p.final_state]
+        for p in final_state: 
+            graph.walk(p, vertex_action=mark, particle_action=mark, ascend=True)
+        
+        maxdistance = max(getattr(p, "distance_to_final_vertex", 0) for p in graph.particles)
+        
+        data = []; A = data.append
+        A("maxdistance = {0}; data = [".format(maxdistance))
+        
+        
+        lines = [edge.spline for edge in layout.edges]
+        from pprint import pprint
+        #pprint(lines)
+        lines = [edge.spline for edge in layout.edges if edge.spline and edge.spline[0] and edge.spline[1]]
+        
+        assert lines, "Something wrong with the layout?"
+        
+        p1s, p2s = zip(*lines)
+        ps = p1s + p2s
+        ps = xs, ys, zs = zip(*ps)
+        cmins = xmin, ymin, zmin = map(min, ps)
+        cmaxs = xmax, ymax, zmax = map(max, ps)
+        diffs = xd, yd, zd = [mi - ma for mi, ma in zip(cmins, cmaxs)]
+        corrections = [d / 2 for d in diffs]
+        
+        def point_correction(p):
+            return [(coord - mi + correction)*2
+                     for coord, correction, mi in zip(p, corrections, cmins)]
+        
+        def correction(spline):
+            p1, p2 = spline
+            return point_correction(p1), point_correction(p2)
+        
+        for edge in layout.edges:
+            if not edge.spline: continue
+            edge.spline = correction(edge.spline)
+        
+        for edge in layout.edges:
+            if not edge.spline: continue
+            
+            sa = edge.style_args
+            A("[{0}, {1}, {2}],".format(
+                ", ".join(map(str, edge.spline[0] + edge.spline[1])),
+                get_color(sa.get("fill", "white")),
+                sa.get("stroke-width", 1),
+            ))
+        
+        for p in []: # graph.particles:
+            lo = object_to_layoutobject[p]
+            sa = lo.style_args
+            color = sa.get("fill", "white")
+            from math import log10
+            #linewidth = sa.get("stroke-width", 1)
+            linewidth = log10(1+p.e)
+            #A("{p.p} {p.e} {p.pdgid} {0.style_args}".format(, p=p))
+            A("[{p.p[0]}, {p.p[1]}, {p.p[2]}, {p.e}, {linewidth}, {color}, {depth}],".format(
+                p=p, color=color, linewidth=round(linewidth, 2), 
+                depth=getattr(p, "distance_to_final_vertex", 0)))
+        
+        A("]")
+        
+        self.write_data("\n".join(data))
+        
+    
+    SCRIPT_TAG = re.compile('<script src="([^"]+)"></script>')
+    def inject_javascript(self, javascript_text):
+        
+        def match(m):
+            (javascript_filename,) = m.groups()
+            args = "mcviz.tools.painters.data", javascript_filename
+            if not resource_exists(*args):
+                return
+            
+            # ]]> is not allowed in CDATA and it appears in jquery!
+            # Try to prevent that..
+            javascript = resource_string(*args).replace("']]>'", "']' + ']>'")
+            
+            stag = '<script type="text/javascript"><![CDATA[\n{0}\n]]></script>'
+            
+            return stag.format(javascript.decode("UTF-8"))
+        
+        return SCRIPT_TAG.sub(match, javascript_text)
