b5145121b3bc9262914d123207218e0c7d82b3e8
Peter Waller <peter.waller@gmail.com>
2011-12-09 17:05:59 +0000

    Add content/ which contains viewers, and a switch to the --painter=3 to control the view mode

--- mcviz/tools/painters/painters.py
+++ mcviz/tools/painters/painters.py
@@ -64,11 +64,10 @@ class DOTPainter(StdPainter, FundamentalTool):
 
 class ThreePainter(StdPainter, FundamentalTool):
     _name = "3"
+    _args = (Arg("mode", str, "3D mode, can be sfdp or momentum", default="sfdp"),)
+    
     def __call__(self, workspace, layout):
-        
-        assert getattr(layout, "is3D", False), (
-            "You must use a layout engine with 3D co-ordinates.")
-        
+                
         from webcolors import name_to_hex
         def get_color(color):
             if not color.startswith("#"):
@@ -95,55 +94,61 @@ class ThreePainter(StdPainter, FundamentalTool):
         data = []; A = data.append
         A("maxdistance = {0}; data = [".format(maxdistance))
         
+        mode = self.options["mode"]
         
-        lines = [edge.spline for edge in layout.edges]
-        from pprint import pprint
-        #pprint(lines)
-        lines = [edge.spline for edge in layout.edges if edge.spline and edge.spline[0] and edge.spline[1]]
-        
-        assert lines, "Something wrong with the layout?"
-        
-        p1s, p2s = zip(*lines)
-        ps = p1s + p2s
-        ps = xs, ys, zs = zip(*ps)
-        cmins = xmin, ymin, zmin = map(min, ps)
-        cmaxs = xmax, ymax, zmax = map(max, ps)
-        diffs = xd, yd, zd = [mi - ma for mi, ma in zip(cmins, cmaxs)]
-        corrections = [d / 2 for d in diffs]
-        
-        def point_correction(p):
-            return [(coord - mi + correction)*2
-                     for coord, correction, mi in zip(p, corrections, cmins)]
-        
-        def correction(spline):
-            p1, p2 = spline
-            return point_correction(p1), point_correction(p2)
-        
-        for edge in layout.edges:
-            if not edge.spline: continue
-            edge.spline = correction(edge.spline)
-        
-        for edge in layout.edges:
-            if not edge.spline: continue
+        if mode == "sfdp":
+            assert getattr(layout, "is3D", False), (
+                "You must use a layout engine with 3D co-ordinates.")
             
-            sa = edge.style_args
-            A("[{0}, {1}, {2}],".format(
-                ", ".join(map(str, edge.spline[0] + edge.spline[1])),
-                get_color(sa.get("fill", "white")),
-                sa.get("stroke-width", 1),
-            ))
+            lines = [edge.spline for edge in layout.edges]
+            from pprint import pprint
+            #pprint(lines)
+            lines = [edge.spline for edge in layout.edges if edge.spline and edge.spline[0] and edge.spline[1]]
+            
+            assert lines, "Something wrong with the layout?"
+            
+            p1s, p2s = zip(*lines)
+            ps = p1s + p2s
+            ps = xs, ys, zs = zip(*ps)
+            cmins = xmin, ymin, zmin = map(min, ps)
+            cmaxs = xmax, ymax, zmax = map(max, ps)
+            diffs = xd, yd, zd = [mi - ma for mi, ma in zip(cmins, cmaxs)]
+            corrections = [d / 2 for d in diffs]
+            
+            def point_correction(p):
+                return [(coord - mi + correction)*2
+                         for coord, correction, mi in zip(p, corrections, cmins)]
+            
+            def correction(spline):
+                p1, p2 = spline
+                return point_correction(p1), point_correction(p2)
+            
+            for edge in layout.edges:
+                if not edge.spline: continue
+                edge.spline = correction(edge.spline)
+            
+            for edge in layout.edges:
+                if not edge.spline: continue
+                
+                sa = edge.style_args
+                A("[{0}, {1}, {2}],".format(
+                    ", ".join(map(str, edge.spline[0] + edge.spline[1])),
+                    get_color(sa.get("fill", "white")),
+                    sa.get("stroke-width", 1),
+                ))
         
-        for p in []: # graph.particles:
-            lo = object_to_layoutobject[p]
-            sa = lo.style_args
-            color = sa.get("fill", "white")
-            from math import log10
-            #linewidth = sa.get("stroke-width", 1)
-            linewidth = log10(1+p.e)
-            #A("{p.p} {p.e} {p.pdgid} {0.style_args}".format(, p=p))
-            A("[{p.p[0]}, {p.p[1]}, {p.p[2]}, {p.e}, {linewidth}, {color}, {depth}],".format(
-                p=p, color=color, linewidth=round(linewidth, 2), 
-                depth=getattr(p, "distance_to_final_vertex", 0)))
+        elif mode == "momentum":
+            for p in graph.particles:
+                lo = object_to_layoutobject[p]
+                sa = lo.style_args
+                color = sa.get("fill", "white")
+                from math import log10
+                #linewidth = sa.get("stroke-width", 1)
+                linewidth = log10(1+p.e)
+                #A("{p.p} {p.e} {p.pdgid} {0.style_args}".format(, p=p))
+                A("[{p.p[0]}, {p.p[1]}, {p.p[2]}, {p.e}, {linewidth}, {color}, {depth}],".format(
+                    p=p, color=color, linewidth=round(linewidth, 2), 
+                    depth=getattr(p, "distance_to_final_vertex", 0)))
         
         A("]")
         
