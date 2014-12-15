2e9c32b57d76718fe6fbc830f1db115fa1405e16
Tim Brooks <brooks@cern.ch>
2014-12-15 21:15:21 +0000

    PEP8-ify bootstrap script

--- mcviz/utils/make_bootstrap.py
+++ mcviz/utils/make_bootstrap.py
@@ -1,12 +1,13 @@
 #! /usr/bin/env python
+"""Produce the mcviz bootstrap script"""
 
 import virtualenv, textwrap
 
-output = virtualenv.create_bootstrap_script(textwrap.dedent("""
+OUTPUT = virtualenv.create_bootstrap_script(textwrap.dedent("""
     import os, subprocess
     from os import symlink
     def after_install(options, home_dir):
-        
+
         os.system("git submodule update --init mcviz.examples")
         os.system("git submodule update --init mcviz.jet")
         try:
@@ -15,18 +16,18 @@ output = virtualenv.create_bootstrap_script(textwrap.dedent("""
             pass
 
         ret = subprocess.call([join(home_dir, 'bin', 'pip'), 'install', '-e', '.'])
-        
+
         try:
             symlink("env/bin/mcviz", "mcv")
         except OSError:
             pass
-        
+
         if not ret:
             print "Finished! Now to run mcviz, type ./mcv"
-    
+
     def adjust_options(options, args):
         options.use_distribute = True
         options.search_dirs.append("dist/")
         args[:] = ["env"]
 """))
-f = open('mcviz_bootstrap.py', 'w').write(output)
+open('mcviz_bootstrap.py', 'w').write(OUTPUT)
