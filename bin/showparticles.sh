809cee1faf3f073dc4b62220e2a4462c69163ef2
Tim Brooks <brooks@cern.ch>
2014-12-16 14:47:39 +0000

    Remove bash scripts

--- bin/showparticles.sh
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env bash
-# Usage: ./showparticles.sh <pythia log> particleno1 particleno2 particleno3
-# Helper script to show only the lines for the given particle numbers.
-# Useful for debugging.
-
-FILE=$1
-shift 1
-PIDS=$(echo $@ | tr ' ' '|')
-grep -E "^[ ]+(no|$PIDS)" $FILE
