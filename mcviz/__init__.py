389a1bee8e88881043ddb880d69afb1dab2eccf8
Tim Brooks <brooks@cern.ch>
2014-12-22 23:41:26 +0000

    Declare mcviz namespace in package init

--- mcviz/__init__.py
+++ mcviz/__init__.py
@@ -1,3 +1,9 @@
+
+try:
+    __import__('pkg_resources').declare_namespace(__name__)
+except ImportError:
+    __path__ = __import__('pkgutil').extend_path(__path__, __name__)
+
 import sys
 
 if not sys.version_info >= (2, 6):
