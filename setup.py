768e431403ca8a8dd7b6d3f3df8b86da38f7bc86
Tim Brooks <brooks@cern.ch>
2014-12-22 02:24:21 +0000

    PEP8-ify setup and declare mcviz as a namespace

--- setup.py
+++ setup.py
@@ -1,32 +1,35 @@
+"""Setuptools config"""
 from setuptools import setup, find_packages
 from textwrap import dedent
 
-version = "2014.12.21"
+VERSION = "2014.12.21"
 
 setup(name='mcviz',
-      version=version,
+      version=VERSION,
       description="mcviz",
       long_description=dedent("""
-          `MCViz` is intended to be a tool for novices to have a quick play 
-          around in, or for more serious users and people who need to explore 
+          `MCViz` is intended to be a tool for novices to have a quick play
+          around in, or for more serious users and people who need to explore
           generator information to make sense of what is going on.
-      """).strip(),      
-      classifiers=[
-        'Development Status :: 1 - Alpha',
-        'Intended Audience :: Physicists :: Developers',
-        'GNU Affero General Public License v3',
-      ],
+      """).strip(),
+      classifiers=['Development Status :: 1 - Alpha',
+                   'Intended Audience :: Physicists :: Developers',
+                   'GNU Affero General Public License v3',
+                  ],
       keywords='mcviz hep montecarlo hepmc graphviz svg visualization',
       author='Johannes Ebke and Peter Waller',
       author_email='dev@mcviz.net',
       url='http://mcviz.net',
       license='Affero GPLv3',
+      namespace_packages=['mcviz'],
       packages=find_packages(),
       entry_points={"console_scripts" : ["mcviz = mcviz:main"]},
-      package_data={
-        "mcviz.utils.svg.data" : ["*.js", "*.xml","ParticleData.xml", "texglyph.cache"]
-      },
-      install_requires=[
-        'argparse',
-      ],
-      )
+      package_data={"mcviz.utils.svg.data" : ["*.js",
+                                              "*.xml",
+                                              "ParticleData.xml",
+                                              "texglyph.cache",
+                                             ]
+                   },
+      install_requires=['argparse',
+                       ],
+     )
